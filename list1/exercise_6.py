cf1b3207af15200ea38d68ad76d61b38e1e26ad7
jacksoncl7 <jacksonlima@id.uff.br>
2021-02-07 13:52:37 -0300

    Added solutions to exercise 6 in list 1

---
+++ list1/exercise_6.py
@@ -0,0 +1,16 @@
+seq = list(map(int, input('digite a sequencia: ').split()))
+aux = seq[0]
+counter = 0
+result = 0
+for num in seq[1:]:
+    if aux != num:
+        counter = 0
+    else:
+        counter += 1
+
+    if counter >= 2:
+        result += 1
+        
+    aux = num
+    print("contador: {} numeral: {} resultado: {}".format(counter, num, result))
+print(result)
